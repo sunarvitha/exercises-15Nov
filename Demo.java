88b21ba88127bf2d898893ccbe4b7bd1101da507
sunarvitha <150986058+sunarvitha@users.noreply.github.com>
2023-11-21 15:39:42 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,26 @@
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		int p=10;
+		do 
+		{
+			System.out.println("Welcome....."+p);
+			p++;/// 11
+		}
+		while(p <=5);// 11 <=5--- false
+        int h=10;
+		while(h<=5)
+		{
+			System.out.println("Hello...."+h);
+			h++;
+		}
+		for(int k=10;k<=5;k++) 
+		{
+			System.out.println("Java....."+k);
+		}
+		System.out.println("Program Ends");
+	}
+
+}
